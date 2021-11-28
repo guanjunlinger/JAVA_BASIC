8799b8b4ab79dca7cd8058afb4924de15af76316
guanjun <1126964622@qq.com>
2021-11-28 21:45:04 +0800

    交错字符串

--- src/main/java/com/algorithm/dp/Solution.java
+++ src/main/java/com/algorithm/dp/Solution.java
@@ -1,48 +1,44 @@
 package com.algorithm.dp;
 
 
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Set;
 
 class Solution {
-    Set<Character> set = new HashSet();
+    private HashMap<Integer, Boolean> map = new HashMap<>();
 
-    {
-        set.add('a');
-        set.add('o');
-        set.add('e');
-        set.add('i');
-        set.add('u');
+    public boolean canIWin(int max, int desired) {
+        int sum = (1 + max) * max / 2;
+        if (sum < desired) return false;
+        if (desired <= 0) return true;
+        return canIWin(max, desired, 0);
     }
 
-    public boolean isVowel(char s) {
-        return set.contains(s);
-    }
-
-    public long countVowels(String word) {
-        int n = word.length();
-        long[] dp = new long[n];
-        if (isVowel(word.charAt(0))) {
-            dp[0] = 1;
+    private boolean canIWin(int max, int desired, int state) {
+        if (desired <= 0) {
+            return false;
         }
-        long sum = dp[0];
-        for (int i = 1; i < n; i++) {
-            if (isVowel(word.charAt(i))) {
-                dp[i] = dp[i - 1] + i + 1;
-            } else {
-                dp[i] = dp[i - 1];
+        if (map.containsKey(state)) return map.get(state);
+        for (int i = 1; i <= max; i++) {
+            if (((1 << (i - 1)) & state) == 0) {
+                boolean win = canIWin(max, desired - i, state | (1 << i - 1));
+                if (!win) {
+                    map.put(state, true);
+                    return true;
+                }
+
             }
-            sum = sum + dp[i];
-
         }
-        return sum;
+        map.put(state, false);
+        return false;
     }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Solution solution = new Solution();
-        String s = scanner.nextLine();
-        System.out.println(solution.countVowels(s));
+        int max = scanner.nextInt();
+        int desire = scanner.nextInt();
+        System.out.println(solution.canIWin(max, desire));
     }
 }
