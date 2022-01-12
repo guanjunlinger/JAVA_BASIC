e9e5fc0b792111577a51a6691df07c9b23b4b597
guanjun <1126964622@qq.com>
2022-01-12 22:49:51 +0800

    动态压缩DP

---
+++ src/main/java/com/algorithm/Solution.java
@@ -0,0 +1,16 @@
+package com.algorithm;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Solution solution = new Solution();
+
+
+    }
+
+
+}
